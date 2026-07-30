b3846c05fffe0bc2155646ec64b19a5a9204e1c5
3st3b4nR <esteban.roldanrm@gmail.com>
2026-07-29 19:30:15 -0500

    configs para aiven

--- server/src/common/configs/db.config.js
+++ server/src/common/configs/db.config.js
@@ -3,7 +3,6 @@ import mysql from "mysql2/promise";
 
 dotenv.config();
 
-// Crear el pool de conexiones
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -21,33 +20,30 @@ const pool = mysql.createPool({
   queueLimit: 0,
   dateStrings: true
 });
-// Función para obtener una conexión
+
 const getConnection = async () => {
   try {
-    const connection = await pool.getConnection();
-    // console.log("Conexión obtenida del pool");
-    return connection;
+    return await pool.getConnection();
   } catch (error) {
     console.error("Error al obtener la conexión:", error);
     throw error;
   }
 };
 
-// Función para liberar una conexión
 const releaseConnection = (connection) => {
-  if (connection) {
-    connection.release(); // Devuelve la conexión al pool
-    // console.log("Conexión liberada al pool");
-  }
+  if (connection) connection.release();
 };
 
-// Función para probar la conexión
 const testConnection = async () => {
   let connection;
+
   try {
     connection = await getConnection();
-    console.log(`Conexión exitosa a la base de datos ${process.env.DB_NAME} en ${process.env.DB_HOST}`);
-    await connection.query("SELECT 1"); // Prueba simple
+    await connection.query("SELECT 1");
+
+    console.log(
+      `Conexión exitosa a ${process.env.DB_NAME} en ${process.env.DB_HOST}:${process.env.DB_PORT}`
+    );
   } catch (error) {
     console.error("Error en la prueba de conexión:", error);
   } finally {
@@ -55,19 +51,22 @@ const testConnection = async () => {
   }
 };
 
-// Función para ejecutar una consulta
 const executeQuery = async (query, params = [], connection) => {
-  let connectionDb = null;
+  let connectionDb;
+
   try {
-    connectionDb = connection ? connection : await getConnection();
+    connectionDb = connection || await getConnection();
     const [results] = await connectionDb.execute(query, params);
     return results;
-  } catch (error) {
-    console.error("Error ejecutando la consulta:", error);
-    throw error;
   } finally {
     if (!connection && connectionDb) releaseConnection(connectionDb);
   }
 };
 
-export { pool, testConnection, getConnection, releaseConnection, executeQuery };
+export {
+  pool,
+  testConnection,
+  getConnection,
+  releaseConnection,
+  executeQuery
+};
